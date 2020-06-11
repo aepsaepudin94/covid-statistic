a0008af2f0e16fe6b2d3f76bf618d8a4af1155f0
Aep Saepudin <aep.saepudin94@gmail.com>
2020-06-11 22:06:33 +0700

    add some plugin and loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,43 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SimpleProgressPlugin = require('webpack-simple-progress-plugin');
 const path = require("path");
 
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
 module.exports = {
-   entry: "./src/index.js",
-   output: {
-       path: path.resolve(__dirname, "dist"),
-       filename: "bundle.js"
-   },
-   module: {
-       rules: [
-           {
-               test: /\.css$/,
-               use: [
-                   {
-                       loader: "style-loader"
-                   },
-                   {
-                       loader: "css-loader"
-                   }
-               ]
-           }
-       ]
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        include: [resolve('src')],
+        loader: 'eslint-loader',
+      }
+    ]
    },
    plugins: [
-       new HtmlWebpackPlugin({
-           template: "./src/index.html",
-           filename: "index.html"
-       })
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new SimpleProgressPlugin(),
    ]
 }
